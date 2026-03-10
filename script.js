8660d93f247b072fbda22961a33826697d81a6ea
Trishia-Soriano <aishtriasoriano@gmail.com>
2026-03-10 18:26:34 +0800

    updated

--- script.js
+++ script.js
@@ -1,5 +1,18 @@
-console.log("The brain is working!");
-function changeColor() {
-    document.body.style.backgroundColor = "#5867f0";
+// we find the button in the HTML and name it 'btn'
+const btn = document.querySelector('button');
 
-}
+//we create a function that runs every time the button is clicked
+btn.addEventListener('click', function() {
+    alert('Button was clicked!');
+});
+
+    //the 'logic': we chek the current color of the body 
+if (document.body.style.backgroundColor === 'lightblue') {
+
+    //if the color is lightblue, we change it to white
+    document.body.style.backgroundColor = 'white';
+} else {
+
+    //if the color is not lightblue, we change it to lightblue
+    document.body.style.backgroundColor = 'lightblue';
+};
